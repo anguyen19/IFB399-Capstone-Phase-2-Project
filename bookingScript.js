779056d22835ab5fc55bb62a489594db747c1700
Andrew Nguyen <anguyen19@protonmail.com>
2021-08-15 21:48:36 +1000

    Linked booking page to home page, basic value parsing from form with javascript created

---
+++ bookingScript.js
@@ -0,0 +1,7 @@
+function testdata() {
+    //alert("this works");
+    let email = document.getElementById("email").value
+    let info = document.getElementById("info").value;
+    let date = document.getElementById("date").value
+    console.log(date);
+}
